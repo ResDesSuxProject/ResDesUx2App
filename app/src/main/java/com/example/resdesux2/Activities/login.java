5d7d5bd219b5b4e14d91f10436f762856e1e4513
sildijkman <s.dijkman@student.utwente.nl>
2022-12-24 23:07:13 +0100

    Move login to activities folder.

--- app/src/main/java/com/example/resdesux2/Activities/login.java
+++ app/src/main/java/com/example/resdesux2/Activities/login.java
@@ -4,30 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.resdesux2.R;
+import com.example.resdesux2.Server.BoundActivity;
 
-public class login extends AppCompatActivity {
+public class login extends BoundActivity {
+
+    private static final String TAG = "login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        findViewById(R.id.button_login).setOnClickListener(buttonClickListener); // VAN main NAAR calendar
-
+        findViewById(R.id.button_login).setOnClickListener(this::loginClicked); // VAN main NAAR calendar
     }
 
-    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.button_login:
-                    Intent intent_login = new Intent(login.this, MainActivity.class);
-                    startActivity(intent_login);
-                    break;
-            }
+    public void loginClicked(View view) {
+        if (isBound) {  // isConnected &&
+            String username = ((TextView) findViewById(R.id.editTextTextPersonName)).getText().toString();
+
+            Log.d(TAG, "loginClicked: " + username);
+            String password = "";
+            serverService.login(username, password, this::loggedIn);
+//        } else {
+//            // TODO show server not connected
+//            loggedIn(0);
         }
-    };
+    }
+
+    public void loggedIn(int userID) {
+        if (userID != -1) {
+            Intent intent_login = new Intent(login.this, MainActivity.class);
+            startActivity(intent_login);
+        } else {
+            // TODO show error message
+        }
+    }
 }
