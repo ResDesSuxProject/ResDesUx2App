cbf25964dab49ea80d1e171848bd00bbdb39b857
Frank <frankbsmn2@gmail.com>
2022-12-26 17:01:23 +0100

    Added feedback to login screen and fixed reading password

--- app/src/main/java/com/example/resdesux2/Activities/LoginActivity.java
+++ app/src/main/java/com/example/resdesux2/Activities/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.resdesux2.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,27 +18,54 @@ public class LoginActivity extends BoundActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        findViewById(R.id.button_login).setOnClickListener(this::loginClicked); // VAN main NAAR calendar
+        findViewById(R.id.login_button).setOnClickListener(this::loginClicked);
     }
 
+    /**
+     * This function is called when the login button is called and handles the interaction with it
+     * @param view the view connected to the button
+     */
     public void loginClicked(View view) {
         if (isBound && isConnected) {  // isConnected &&
-            String username = ((TextView) findViewById(R.id.editTextTextPersonName)).getText().toString();
+            String username = ((TextView) findViewById(R.id.login_username_input)).getText().toString().trim();
+            String password = ((TextView) findViewById(R.id.login_password_input)).getText().toString().trim();
 
-            Log.d(TAG, "loginClicked: " + username);
-            String password = "";
+            if (username.length() == 0) {
+                ((TextView) findViewById(R.id.login_username_input)).setError("Username is empty");
+                return;
+            }
+
             serverService.login(username, password, this::loggedIn);
         } else {
             Toast.makeText(this, "Server not connected", Toast.LENGTH_SHORT).show();
         }
     }
 
+    /**
+     * Called when the server responed on the login request
+     * userID of -1 means that it failed, anything else is the userID of the user
+     * @param userID The users ID, -1 means failed to login.
+     */
     public void loggedIn(int userID) {
         if (userID != -1) {
             Intent intent_login = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent_login);
         } else {
-            Toast.makeText(this, "Username incorrect", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Username is incorrect", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    protected void onConnected(boolean connected) {
+        super.onConnected(connected);
+        findViewById(R.id.login_button).setEnabled(true);
+        findViewById(R.id.login_not_connected_txt).setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onFailedConnection(boolean isConnected) {
+        super.onFailedConnection(isConnected);
+        findViewById(R.id.login_button).setEnabled(false);
+        findViewById(R.id.login_not_connected_txt).setVisibility(View.VISIBLE);
     }
 }
