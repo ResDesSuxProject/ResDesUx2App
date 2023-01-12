4eb517866c2beb31d92df51f7d5cc8ab3e7588f8
Frank <frankbsmn2@gmail.com>
2023-01-12 14:58:44 +0100

    Added gif to the home page

--- app/src/main/java/com/example/resdesux2/Activities/MainActivity.java
+++ app/src/main/java/com/example/resdesux2/Activities/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.resdesux2.Activities;
 
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory;
 import com.example.resdesux2.Models.User;
 import com.example.resdesux2.R;
 import com.example.resdesux2.Server.BoundActivity;
@@ -45,6 +54,14 @@ public class MainActivity extends BoundActivity {
         TextView textView = (TextView) findViewById(R.id.scoreView);
         textView.setText(String.format(Locale.US, "Your scores are %d and %d",
                 intensityScore, frequencyScore));
+
+        // Show gif with rounded corners and fade
+        ImageView imageView = (ImageView) findViewById(R.id.my_image_view);
+        Glide.with(this)
+                .load(R.drawable.dagoesleep1)
+                .transform(new CenterCrop(),new RoundedCorners(14))
+                .transition(withCrossFade(new DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()))
+                .into(imageView);
     }
 
     private void onUserRetrieved(User currentUser) {
