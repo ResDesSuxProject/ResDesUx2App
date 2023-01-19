5bfb1a5c8eb5cc2c094289ae101c1e01b535f8a2
sildijkman <s.dijkman@student.utwente.nl>
2023-01-19 14:51:45 +0100

    The infopage is ready

--- app/src/main/java/com/example/resdesux2/Activities/InfoActivity.java
+++ app/src/main/java/com/example/resdesux2/Activities/InfoActivity.java
@@ -2,7 +2,10 @@ package com.example.resdesux2.Activities;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
 
+import com.daimajia.numberprogressbar.NumberProgressBar;
 import com.example.resdesux2.NavigationFragment;
 import com.example.resdesux2.R;
 import com.example.resdesux2.Server.BoundActivity;
@@ -18,5 +21,21 @@ public class InfoActivity extends BoundActivity {
         NavigationFragment navigationFragment = NavigationFragment.newInstance(NavigationFragment.Options.Insight, this::navigateTo);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.navigationFragmentContainer, navigationFragment).commit();
+    }
+
+    public void setIndeterminate (boolean indeterminate){
+
+    }
+
+    @Override
+    protected void onScoreChange(int intensityScore, int frequencyScore) {
+        super.onScoreChange(intensityScore, frequencyScore);
+        NumberProgressBar bar_intensity = findViewById(R.id.determinateBar_intensity);
+        NumberProgressBar bar_frequency = findViewById(R.id.determinateBar_frequency);
+        bar_intensity.setProgress(intensityScore);
+        bar_frequency.setProgress(frequencyScore);
+
+
+
     }
 }
